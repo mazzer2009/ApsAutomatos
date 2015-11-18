c7884a9735fa079b807d6d9303745180b6a28601
MarcoC <MarcoC@192.168.25.106>
2015-11-18 00:49:49 -0200

    Merge origin/master
    Conflicts:
    	src/apsautomatos/Main.java

--- src/apsautomatos/Main.java
+++ src/apsautomatos/Main.java
@@ -18,57 +18,7 @@ public class Main {
         Linguagem ling = new Linguagem();
         arq.lerArquivo("unitario", ling);
 
-<<<<<<< HEAD
-        arq.lerArquivo("teste", ling);
-       ling.imprimeTrans();
-       ling.removeInuteis();
-//       ling.removeNulls();
-       ling.imprimeTrans();
-       
-       
-        
+        ling.removeUnitario();
 
-=======
-      //  arq.lerArquivo("teste", ling);
-        ArrayList<Transicao> t = ling.getListaTransicao();
-        ArrayList<Simbolo> ts = new ArrayList<>();
-        ArrayList<Transicao> tt = new ArrayList<>();
-        int i = 0;
-//        ts = ling.removeInuteis(t);
-//        for (Simbolo nova : ts) {
-//            System.out.println(nova.getNome());
-//        }
-          ling.removeUnitario();
-          int j=0;
-          for(Transicao trans:t){
-              System.out.println(" ");
-              for(j=0;j<trans.getListaDestino().size();j++){
-              System.out.println(trans.getOrigem().getNome()+ " para "+ trans.getListaDestino().get(j).getNome());
-//                  
-              }
-//        
-        
-//        for (Transicao tt : ling.getListaTransicao()) {
-//            System.out.print("\nOri: " + tt.getOrigem().getNome() + "----->");
-//            for (Simbolo ss : tt.getListaDestino()) {
-//
-//                System.out.print("" + ss.getNome());
-//
-//            }
-//        System.out.println("\n--------------------");
-//        ArrayList<Simbolo> ll = ling.procuraNulls();
-//        for (Simbolo s : ll) {
-//            System.out.println("S: " + s.getNome());
-//        }
-//        System.out.println("---1---");
-//        ArrayList<Transicao> tt = ling.procuraTransDestNull(ll);
-//        for (Transicao tss : tt) {
-//            System.out.println("" + tss.getOrigem().getNome());
-//        }
-//        System.out.println("--------------------");
-//    }
->>>>>>> origin/master
     }
-}
-
 }
