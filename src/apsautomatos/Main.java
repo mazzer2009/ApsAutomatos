d8808e8367e9737830c80c2900f88dba20b34ef8
MarcoC <MarcoC@192.168.25.106>
2015-11-18 00:47:36 -0200

    Merge origin/master
    Conflicts:
    	src/View/GUI.java

--- src/apsautomatos/Main.java
+++ src/apsautomatos/Main.java
@@ -18,38 +18,13 @@ public class Main {
         Linguagem ling = new Linguagem();
 
         arq.lerArquivo("teste", ling);
-        ArrayList<Transicao> t = ling.getListaTransicao();
-        ArrayList<Transicao> ts = new ArrayList<>();
-        int i = 0;
-        for (Transicao tt : ling.getListaTransicao()) {
-            System.out.print("\nOri: " + tt.getOrigem().getNome() + "----->");
-            for (Simbolo ss : tt.getListaDestino()) {
-
-                System.out.print("" + ss.getNome());
-
-            }
-        }
-        System.out.println("\n--------------------");
-        ArrayList<Simbolo> ll = ling.procuraNulls();
-        for (Simbolo s : ll) {
-            System.out.println("S: " + s.getNome());
-        }
-        System.out.println("---1---");
-        ArrayList<Transicao> tt = ling.procuraTransDestNull(ll);
-        for(Transicao tss : tt){
-            System.out.println(""+tss.getOrigem().getNome());
-        }
-        System.out.println("--------------------");
-        ts = ling.removeInuteis(t);
-        for (Transicao nova : ts) {
-            System.out.println(" ");
-            for (i = 0; i < nova.getListaDestino().size(); i++) {
-                //System.out.println(nova.getListaDestino().size());
-                System.out.println(nova.getOrigem().getNome() + "----->" + nova.getListaDestino().get(i).getNome());
-            }
-            //s.add(nova.getListaDestino())
-
-        }
+       ling.imprimeTrans();
+       ling.removeInuteis();
+//       ling.removeNulls();
+       ling.imprimeTrans();
+       
+       
+        
 
     }
 
